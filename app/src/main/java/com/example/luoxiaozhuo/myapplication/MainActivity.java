0a99b5d56c557071515029effcad43fe0f8849bb
luoxiaozhuo <lxz8023@126.com>
2018-03-29 15:35:01 +0800

    test

--- app/src/main/java/com/example/luoxiaozhuo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/luoxiaozhuo/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_notifications + " hello world!") ;
                     return true;
             }
             return false;
